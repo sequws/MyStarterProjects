49fd5d525116020f0b3b139dc9ba6cf8ad3256fd
sequws <workspaceshare@gmail.com>
2022-01-15 13:13:09 +0100

    Add WpfPrismMahAppsTemplate - template for simple WPF apps based on MVVM

---
+++ WpfPrismMahAppsTemplate/WpfPrismMahAppsTemplate/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using WpfPrismMahAppsTemplate.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using ControlzEx.Theming;
+using System.Threading;
+using Prism.Regions;
+
+namespace WpfPrismMahAppsTemplate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            var lang = WpfPrismMahAppsTemplate.Properties.Settings.Default.AppLanguage;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<DemoView1>("Demo1View");
+
+            // Set view in region 
+            var regionManager = Container.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(DemoView1));
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the application theme to Dark.Green
+            //ThemeManager.Current.ChangeTheme(this, "Dark.Green");
+        }
+    }
+}
---
+++ WpfPrismMahAppsTemplate/WpfPrismMahAppsTemplate/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using Prism.Regions;
+
+namespace WpfPrismMahAppsTemplate.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
